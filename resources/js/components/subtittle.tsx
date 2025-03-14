e41ee03c7b063dd73ad3d92070cf68885d9f496b
Guille <guillermolabordagomez10@gmail.com>
2025-03-14 19:00:41 +0100

    fix: :bug: Cambiar valores por defecto

--- resources/js/components/subtittle.tsx
+++ resources/js/components/subtittle.tsx
@@ -1,12 +1,12 @@
 import { Link } from '@inertiajs/react'
 import { ArrowLeft } from 'lucide-react'
 
-export default function Subtitle({ needBack, subtitulo, pagAnterior }: { needBack: boolean; subtitulo: string; pagAnterior: string }) {
+export default function Subtitle({ needBack = false, subtitulo, pagAnterior = "" }: { needBack: boolean; subtitulo: string; pagAnterior: string }) {
   return (
     <div className="bg-negro relative flex justify-center py-4">
       {needBack && (
         <Link className="cursor-pointer" href={pagAnterior}>
-          <ArrowLeft size={26} className="hover:bg-blanco absolute left-4 rounded-sm transition-all hover:scale-125 hover:text-black" />
+          <ArrowLeft size={26} className=" text-blanco hover:bg-blanco absolute left-4 rounded-sm transition-all hover:scale-125 hover:text-black" />
         </Link>
       )}
       <h1 className="text-amarillo">{subtitulo}</h1>
