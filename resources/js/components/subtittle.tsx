193ef4a91a7703d28c5428dd111f1d65de145055
Guille <guillermolabordagomez10@gmail.com>
2025-03-14 18:50:56 +0100

    fix: :pencil2: Corregir subtitles

--- resources/js/components/subtittle.tsx
+++ resources/js/components/subtittle.tsx
@@ -1,23 +1,15 @@
-
 import { Link } from '@inertiajs/react'
 import { ArrowLeft } from 'lucide-react'
 
-
-export default function Subtittle({needBack ,titulo, pagAnterior }: {needBack:boolean, titulo: string , pagAnterior:string}) {
-
+export default function Subtitle({ needBack, subtitulo, pagAnterior }: { needBack: boolean; subtitulo: string; pagAnterior: string }) {
   return (
-
-    <div className="relative flex bg-negro py-4 justify-center" >
-      {
-        needBack &&
-        <Link className="cursor-pointer"href={pagAnterior}>
-        <ArrowLeft 
-        size={26}
-        className='rounded-sm absolute left-4 hover:scale-125 hover:bg-blanco hover:text-black transition-all'/>
+    <div className="bg-negro relative flex justify-center py-4">
+      {needBack && (
+        <Link className="cursor-pointer" href={pagAnterior}>
+          <ArrowLeft size={26} className="hover:bg-blanco absolute left-4 rounded-sm transition-all hover:scale-125 hover:text-black" />
         </Link>
-      }
-      <h1 className="text-amarillo "  >{titulo}</h1>
+      )}
+      <h1 className="text-amarillo">{subtitulo}</h1>
     </div>
   )
 }
-
