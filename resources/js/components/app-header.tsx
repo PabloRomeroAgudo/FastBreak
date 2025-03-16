df6e4a0b4a183c3f82d84d44423c138f3581912a
davidlply <davidlply@gmail.com>
2025-03-16 12:54:48 +0100

    feat: :sparkles: Añadir componente header

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -1,89 +1,88 @@
 import { Breadcrumbs } from '@/components/breadcrumbs'
 import { Icon } from '@/components/icon'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { NavigationMenu, NavigationMenuItem, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import { UserMenuContent } from '@/components/user-menu-content'
-import { useInitials } from '@/hooks/use-initials'
-import { cn } from '@/lib/utils'
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types'
 import { Link, usePage } from '@inertiajs/react'
-import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react'
+import { CircleUserRound, Menu, ShoppingCart } from 'lucide-react'
 import AppLogo from './app-logo'
 import AppLogoIcon from './app-logo-icon'
+import Subtitle from './subtitle'
 
-const mainNavItems: NavItem[] = [
-  {
-    title: 'Dashboard',
-    url: '/dashboard',
-    icon: LayoutGrid,
-  },
-]
-
+type NavItemNotAuth = {
+  title: string
+  url: string
+  style: string
+}
 const rightNavItems: NavItem[] = [
   {
-    title: 'Repository',
-    url: 'https://github.com/laravel/react-starter-kit',
-    icon: Folder,
+    title: 'Carrito',
+    url: '/carrito',
+    icon: ShoppingCart,
   },
   {
-    title: 'Documentation',
+    title: 'Perfil',
     url: 'https://laravel.com/docs/starter-kits',
-    icon: BookOpen,
+    icon: CircleUserRound,
   },
 ]
 
-const activeItemStyles = 'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100'
+const rightNavItemsNotAuth: NavItemNotAuth[] = [
+  {
+    title: 'Log in',
+    url: '/login',
+    style: 'text-negro bg-amarillo w-20 rounded-sm text-lg font-bold text-center',
+  },
+  {
+    title: 'Sign in',
+    url: '/register',
+    style: 'text-blanco',
+  },
+]
 
 interface AppHeaderProps {
   breadcrumbs?: BreadcrumbItem[]
+  subtitulo: string
+  needBack?: boolean
+  url?: string
 }
 
-export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
+export function AppHeader({ breadcrumbs = [], subtitulo, needBack = false, url = '' }: AppHeaderProps) {
   const page = usePage<SharedData>()
   const { auth } = page.props
-  const getInitials = useInitials()
+  console.log(auth)
   return (
     <>
-      <div className="border-sidebar-border/80 bg-negro">
-        <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
+      <div className='border-sidebar-border/80 bg-negro'>
+        <div className='mx-auto flex h-16 items-center px-4'>
           {/* Mobile Menu */}
-          <div className="lg:hidden">
+          <div className='lg:hidden'>
             <Sheet>
               <SheetTrigger asChild>
-                <Button variant="ghost" size="icon" className="mr-2 h-[34px] w-[34px]">
-                  <Menu className="h-5 w-5" />
+                <Button variant='ghost' size='icon' className='mr-2 h-[34px] w-[34px]'>
+                  <Menu className='h-5 w-5' />
                 </Button>
               </SheetTrigger>
-              <SheetContent side="left" className="bg-sidebar flex h-full w-64 flex-col items-stretch justify-between">
-                <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
-                <SheetHeader className="flex justify-start text-left">
-                  <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
+              <SheetContent side='left' className='bg-sidebar flex h-full w-64 flex-col items-stretch justify-between'>
+                <SheetTitle className='sr-only'>Navigation Menu</SheetTitle>
+                <SheetHeader className='flex justify-start text-left'>
+                  <AppLogoIcon className='h-6 w-6 fill-current text-black dark:text-white' />
                 </SheetHeader>
-                <div className="flex h-full flex-1 flex-col space-y-4 p-4">
-                  <div className="flex h-full flex-col justify-between text-sm">
-                    <div className="flex flex-col space-y-4">
-                      {mainNavItems.map((item) => (
-                        <Link key={item.title} href={item.url} className="flex items-center space-x-2 font-medium">
-                          {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
-                          <span>{item.title}</span>
-                        </Link>
-                      ))}
-                    </div>
+                <div className='flex h-full flex-1 flex-col space-y-4 p-4'>
+                  <div className='flex h-full flex-col justify-between text-sm'>
+                    <div className='flex flex-col space-y-4'></div>
 
-                    <div className="flex flex-col space-y-4">
+                    <div className='flex flex-col space-y-4'>
                       {rightNavItems.map((item) => (
                         <a
                           key={item.title}
                           href={item.url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="flex items-center space-x-2 font-medium"
+                          target='_blank'
+                          rel='noopener noreferrer'
+                          className='flex items-center space-x-2 font-medium'
                         >
-                          {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
+                          {item.icon && <Icon iconNode={item.icon} className='h-5 w-5' />}
                           <span>{item.title}</span>
                         </a>
                       ))}
@@ -94,91 +93,57 @@ export function AppHeader({ breadcrumbs = [] }: AppHeaderProps) {
             </Sheet>
           </div>
 
-          <Link href="/dashboard" prefetch className="flex items-center space-x-2 text-white">
-            <AppLogo />
-          </Link>
+          <AppLogo />
 
           {/* Desktop Navigation */}
-          <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
-            <NavigationMenu className="flex h-full items-stretch">
-              <NavigationMenuList className="flex h-full items-stretch space-x-2">
-                {mainNavItems.map((item, index) => (
-                  <NavigationMenuItem key={index} className="relative flex h-full items-center">
-                    <Link
-                      href={item.url}
-                      className={cn(
-                        navigationMenuTriggerStyle(),
-                        page.url === item.url && activeItemStyles,
-                        'h-9 cursor-pointer bg-black px-3 text-white',
-                      )}
-                    >
-                      {item.icon && <Icon iconNode={item.icon} className="mr-2 h-4 w-4" />}
-                      {item.title}
-                    </Link>
-                    {page.url === item.url && <div className="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"></div>}
-                  </NavigationMenuItem>
-                ))}
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
 
-          <div className="ml-auto flex items-center space-x-2">
-            <div className="relative flex items-center space-x-1">
-              <Button variant="ghost" size="icon" className="group text-blanco h-9 w-9 cursor-pointer">
-                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
-              </Button>
-              <div className="hidden lg:flex">
-                {rightNavItems.map((item) => (
-                  <TooltipProvider key={item.title} delayDuration={0}>
-                    <Tooltip>
-                      <TooltipTrigger>
-                        <a
-                          href={item.url}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="group ring-offset-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring text-blanco ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
-                        >
-                          <span className="sr-only">{item.title}</span>
-                          {item.icon && <Icon iconNode={item.icon} className="size-5 opacity-80 group-hover:opacity-100" />}
-                        </a>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>{item.title}</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                ))}
+          <div className='ml-auto flex items-center space-x-3'>
+            <div className='relative flex items-center space-x-1'>
+              <div className='hidden lg:flex'>
+                {auth.user ? (
+                  <>
+                    {rightNavItems.map((item) => (
+                      <TooltipProvider key={item.title} delayDuration={0}>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <a
+                              href={item.url}
+                              target='_blank'
+                              rel='noopener noreferrer'
+                              className='group ring-offset-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-ring text-amarillo ml-1 inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50'
+                            >
+                              <span className='sr-only'>{item.title}</span>
+                              {item.icon && <Icon iconNode={item.icon} className='size-5 opacity-80 group-hover:opacity-100' />}
+                            </a>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>{item.title}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ))}
+                    <Link as='button' method='post' href='/logout' className='text-amarillo'>
+                      Cerrar Sesión
+                    </Link>
+                  </>
+                ) : (
+                  <div className='flex items-center gap-4'>
+                    {rightNavItemsNotAuth.map((item) => (
+                      <Link href={item.url} className={item.style}>
+                        {item.title}
+                      </Link>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="size-10 rounded-full p-1">
-                  <Avatar className="size-8 overflow-hidden rounded-full">
-                    {auth.user ? (
-                      <>
-                        <AvatarImage src={auth.user.avatar} alt={auth.user.name} />
-                        <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                          {getInitials(auth.user.name)}
-                        </AvatarFallback>
-                      </>
-                    ) : (
-                      <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                        {getInitials('Pablo Romero')}
-                      </AvatarFallback>
-                    )}
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end">
-                <UserMenuContent user={auth.user} />
-              </DropdownMenuContent>
-            </DropdownMenu>
           </div>
         </div>
       </div>
+      <Subtitle subtitulo={subtitulo} needBack={needBack} url={url} />
       {breadcrumbs.length > 1 && (
-        <div className="border-sidebar-border/70 flex w-full border-b">
-          <div className="mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl">
+        <div className='border-sidebar-border/70 flex w-full border-b'>
+          <div className='mx-auto flex h-12 w-full items-center justify-start px-4 text-neutral-500 md:max-w-7xl'>
             <Breadcrumbs breadcrumbs={breadcrumbs} />
           </div>
         </div>
