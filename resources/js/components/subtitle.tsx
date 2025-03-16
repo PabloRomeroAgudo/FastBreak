594ab17b85e917b1b1aa1c4767a974c355a76d38
Pablo Romero <pabloromeroagudo@gmail.com>
2025-03-16 20:51:26 +0100

    refactor:

--- resources/js/components/subtitle.tsx
+++ resources/js/components/subtitle.tsx
@@ -3,11 +3,17 @@ import { ArrowLeft } from 'lucide-react'
 
 export default function Subtitle({ needBack = false, subtitulo, url = '' }: { needBack: boolean; subtitulo: string; url: string }) {
   return (
-    <div className='bg-negro relative flex justify-center py-4'>
+    <div className='bg-negro relative mb-4 flex justify-center py-4'>
       {needBack &&
         (url ? (
-          <Link className='cursor-pointer' href={url}>
-            <ArrowLeft size={26} className='text-blanco hover:bg-blanco absolute left-4 rounded-sm transition-all hover:scale-125 hover:text-black' />
+          <Link
+            className='cursor-pointer'
+            href={url}
+          >
+            <ArrowLeft
+              size={26}
+              className='text-blanco hover:bg-blanco absolute left-4 rounded-sm transition-all hover:scale-125 hover:text-black'
+            />
           </Link>
         ) : (
           <h2 className='font-principal text-red-600'>No has añadido el link</h2>
