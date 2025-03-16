49db7dadedf8de16ce1b3356fd57d0fbf9a27bbd
davidlply <davidlply@gmail.com>
2025-03-16 12:55:41 +0100

    style: :lipstick: Cambiar estilos del logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -6,8 +6,8 @@ export default function AppLogo() {
       <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
         <AppLogoIcon className="text-amarillo size-5 fill-current dark:text-black" />
       </div>
-      <div className="ml-1 grid flex-1 text-left text-sm">
-        <span className="text-amarillo mb-0.5 truncate leading-none font-semibold">FastBreak</span>
+      <div className="ml-1 grid flex-1 text-left text-3xl">
+        <span className="text-amarillo mb-0.5 truncate leading-none font-principal ">FastBreak</span>
       </div>
     </>
   )
