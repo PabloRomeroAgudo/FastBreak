8a49de75598670732ebd692df12cc55a8c038658
Pablo Romero <pabloromeroagudo@gmail.com>
2025-03-17 17:39:02 +0100

    refactor: :fire: Borrar debugger

--- resources/js/pages/Categorias/categoria.tsx
+++ resources/js/pages/Categorias/categoria.tsx
@@ -2,6 +2,7 @@ import CardProducto from '@/components/producto/cardProducto'
 import AppLayout from '@/layouts/app-layout'
 import { type Producto } from '@/types'
 import { Head } from '@inertiajs/react'
+import { Toaster } from 'sonner'
 
 interface Props {
   categoria: string
@@ -9,13 +10,6 @@ interface Props {
 }
 
 export default function Categoria({ categoria, productos }: Props) {
-  // const contexto = useContext(CarritoContext)
-
-  // if (!contexto) {
-  //   throw new Error('useCarrito debe usarse dentro de un CarritoProvider')
-  // }
-  // const { carrito } = contexto
-
   return (
     <AppLayout
       subtitulo={categoria}
@@ -28,19 +22,19 @@ export default function Categoria({ categoria, productos }: Props) {
       <section className='grid grid-cols-[repeat(auto-fit,minmax(12rem,20rem))] justify-center gap-4 px-8'>
         {productos &&
           productos.map((producto) => {
-            return <CardProducto producto={producto} />
+            return (
+              <CardProducto
+                key={producto.id}
+                producto={producto}
+              />
+            )
           })}
       </section>
 
-      {/* Mostrar carrito */}
-      {/* <p>{carrito.precioTotal}</p>
-      {carrito.productos.map((producto) => {
-        return (
-          <p key={producto.id}>
-            {producto.nombre} {producto.precio}€ {producto.cantidad}
-          </p>
-        )
-      })} */}
+      <Toaster
+        richColors
+        duration={1200}
+      />
     </AppLayout>
   )
 }
