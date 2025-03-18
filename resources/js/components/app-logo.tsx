d678f1b0ab22611771954308389e8cea243081b5
davidlply <davidlply@gmail.com>
2025-03-18 14:32:25 +0100

    feat: :sparkles: Añadir cambios a AppHeader

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,16 +1,20 @@
+import { Link } from '@inertiajs/react'
 import AppLogoIcon from './app-logo-icon'
 
 export default function AppLogo() {
   return (
-    <>
+    <Link
+      method='get'
+      href='/categoria'
+    >
       <div className='flex'>
         <div className='text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md'>
           <AppLogoIcon className='text-amarillo size-5 fill-current dark:text-black' />
         </div>
         <div className='ml-1 grid text-left text-3xl'>
-          <span className='text-amarillo font-principal mb-0.5 truncate leading-none'>FastBreak</span>
+          <h1 className='text-amarillo font-principal mb-0.5 truncate leading-none'>FastBreak</h1>
         </div>
       </div>
-    </>
+    </Link>
   )
 }
