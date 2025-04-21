d0d9eb5cc2774fcd485fbe270bd8012d9e74a9cc
Pablo Romero <pabloromeroagudo@gmail.com>
2025-04-21 20:56:38 +0200

    feat: :lipstick: Arreglar y añadir estilos a los pedidos

--- resources/js/components/pedidos/pedidos.tsx
+++ resources/js/components/pedidos/pedidos.tsx
@@ -1,21 +1,33 @@
 import { Pedido } from '@/types'
+import { ArrowRight } from 'lucide-react'
 import ElemPedido from './elemPedido'
 
 interface Props {
   pedidos: Pedido[]
+  isPrepare?: boolean
 }
 
-export default function Pedidos({ pedidos }: Props) {
+export default function Pedidos({ pedidos, isPrepare = false }: Props) {
   return pedidos.length > 0 ? (
     <section className='grid gap-2'>
       {pedidos.map((pedido) => {
         return (
-          <details className='bg-negro group p-3 text-white'>
-            <summary className='font-principal text-3xl'>
-              <span className='text-amarillo group-open:hidden'>Pedido {pedido.id}</span>
+          <details
+            className='bg-negro group p-3 text-white'
+            key={pedido.id}
+          >
+            <summary className='font-principal flex cursor-pointer list-none items-center gap-2 text-3xl'>
+              <ArrowRight
+                size={30}
+                className='transition-transform duration-300 group-open:rotate-90'
+              />
+              <span className='text-amarillo group-open:scale-0'>Codigo: {pedido.codigo}</span>
             </summary>
 
-            <ElemPedido pedido={pedido} />
+            <ElemPedido
+              pedido={pedido}
+              isPrepare={isPrepare}
+            />
           </details>
         )
       })}
