bf6374a8312599cb67f14c9e9aea5d6acdc9b2ae
Pablo Romero <pabloromeroagudo@gmail.com>
2025-05-08 21:22:48 +0200

    feat: :sparkles: Añadir iconos e imagenes, ajustar logo y poner links en el head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,6 +33,12 @@
 
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+  <link rel="icon" type="image/png" href="/ico/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/ico/favicon.svg" />
+  <link rel="shortcut icon" href="/ico/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/ico/apple-touch-icon.png" />
+  <link rel="manifest" href="/ico/site.webmanifest" />
+
   @routes
   @viteReactRefresh
   @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
