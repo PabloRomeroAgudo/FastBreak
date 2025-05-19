a32818cfdccdbfc0c80d5c74ccf7cb7b8683686c
Pablo Romero <pabloromeroagudo@gmail.com>
2025-05-19 17:46:36 +0200

    feat: :sparkles: Añadir polling de los pedidos

--- resources/js/pages/Pedidos/deliver.tsx
+++ resources/js/pages/Pedidos/deliver.tsx
@@ -2,13 +2,13 @@ import Nav, { LinkValues } from '@/components/pedidos/nav'
 import Pedidos from '@/components/pedidos/pedidos'
 import AppLayout from '@/layouts/app-layout'
 import { Pedido } from '@/types'
-import { Head } from '@inertiajs/react'
+import { Head, usePage, usePoll } from '@inertiajs/react'
 
-interface Props {
-  pedidos: Pedido[]
-}
+export default function Deliver() {
+  const { pedidos } = usePage<{ pedidos: Pedido[] }>().props
 
-export default function Deliver({ pedidos }: Props) {
+  usePoll(40000, { only: ['pedidos'] })
+
   return (
     <AppLayout
       subtitulo='Pedidos'
