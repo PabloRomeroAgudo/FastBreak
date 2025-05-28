70f6d0dcf8bdd6a91dfd411dff680e801bf696e2
Pablo Romero <pabloromeroagudo@gmail.com>
2025-05-28 09:47:27 +0200

    fix: :lipstick: Cambiar responsive de misPedidos

--- resources/js/pages/Categoria/misPedidos.tsx
+++ resources/js/pages/Categoria/misPedidos.tsx
@@ -23,7 +23,7 @@ export default function MisPedidos({ transacciones }: Props) {
             className={`group p-3 text-white open:pb-12 ${t.estado === 'entregado' ? 'bg-gray-500' : 'bg-negro'}`}
             key={t.id}
           >
-            <summary className='font-principal flex cursor-pointer list-none items-center gap-2 text-xl md:text-3xl'>
+            <summary className='font-principal flex cursor-pointer list-none items-center gap-2 text-lg sm:text-xl md:text-3xl'>
               <ArrowRight
                 size={30}
                 className='transition-transform duration-300 group-open:rotate-90'
@@ -31,7 +31,9 @@ export default function MisPedidos({ transacciones }: Props) {
 
               <div className='grid grow grid-cols-3 items-center text-center transition-opacity group-open:opacity-0'>
                 <h3>Pedido {index + 1}</h3>
-                <span className={`text-3xl md:text-5xl ${t.estado === 'entregado' ? 'text-negro' : 'text-amarillo'} `}>Código: {t.codigo}</span>
+                <span className={`text-xl sm:text-3xl md:text-5xl ${t.estado === 'entregado' ? 'text-negro' : 'text-amarillo'} `}>
+                  Código: {t.codigo}
+                </span>
                 <span>
                   {t.fecha} {t.hora}
                 </span>
@@ -39,25 +41,25 @@ export default function MisPedidos({ transacciones }: Props) {
             </summary>
 
             <article className='font-principal text-blanco flex flex-col justify-between gap-4 text-xl opacity-0 transition-opacity group-open:opacity-100 md:px-4 lg:flex-row'>
-              <span className={`self-center text-3xl text-nowrap md:text-5xl ${t.estado === 'entregado' ? 'text-negro' : 'text-amarillo'}`}>
+              <span className={`self-center text-3xl text-nowrap sm:text-5xl ${t.estado === 'entregado' ? 'text-negro' : 'text-amarillo'}`}>
                 Codigo: {t.codigo}
               </span>
-              <span className='self-center text-center text-3xl'>{t.estado}</span>
+              <span className='self-center text-center text-xl sm:text-3xl'>{t.estado}</span>
               <div className='flex flex-col gap-2 self-center'>
                 {t.productos.map((producto) => {
                   return (
                     <label
                       key={`${producto.pivot.id_transaccion}-${producto.pivot.id_producto}`}
-                      className={`relative grid grid-cols-[auto_auto_1fr] items-end gap-5 px-2 text-3xl ${t.productos.length > 1 && 'border-b'}`}
+                      className={`relative grid grid-cols-[1fr_auto_auto] items-end gap-5 px-2 text-xl sm:text-3xl ${t.productos.length > 1 && 'border-b'}`}
                     >
-                      <span className='w-60'>{producto.nombre}</span>
+                      <span className='w-full sm:w-60'>{producto.nombre}</span>
                       <span className='text-nowrap'> {producto.pivot.cantidad} Uds</span>
                       <span className='grow text-end'>{producto.precio}€</span>
                     </label>
                   )
                 })}
               </div>
-              <span className='flex flex-wrap justify-center gap-2 self-center text-3xl'>
+              <span className='flex flex-wrap justify-center gap-2 self-center text-xl sm:text-3xl'>
                 Total:<span className='text-green-500'>{t.total}€</span>
               </span>
             </article>
